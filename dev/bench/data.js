f51b372403103d1d2a9beb0e317de60e65241d51
github-action-benchmark <github@users.noreply.github.com>
2022-09-23 10:56:07 +0000

    add Go Benchmark (go) benchmark result for d27758800ee09f1dec624486582a69327ff0184a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663920980193,
+  "lastUpdate": 1663930567366,
   "repoUrl": "https://github.com/kaihendry/lesshero",
   "entries": {
     "Go Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 175451,
             "unit": "ns/op",
             "extra": "6921 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hendry@iki.fi",
+            "name": "Kai Hendry",
+            "username": "kaihendry"
+          },
+          "committer": {
+            "email": "hendry@iki.fi",
+            "name": "Kai Hendry",
+            "username": "kaihendry"
+          },
+          "distinct": true,
+          "id": "d27758800ee09f1dec624486582a69327ff0184a",
+          "message": "Output JSON",
+          "timestamp": "2022-09-23T18:55:28+08:00",
+          "tree_id": "2be6eff5acdefcee62eb8ce16874461ba9b59c8b",
+          "url": "https://github.com/kaihendry/lesshero/commit/d27758800ee09f1dec624486582a69327ff0184a"
+        },
+        "date": 1663930566597,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkHero",
+            "value": 176347,
+            "unit": "ns/op",
+            "extra": "6964 times\n2 procs"
           }
         ]
       }
